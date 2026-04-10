ea342a036abc38e61bdc785008f257b34bf65699
Antonis Kalou <kalouantonis@protonmail.com>
2026-04-10 18:10:18 +0300

    Add 00-load test

--- lib/Test/QuickGen.pm
+++ lib/Test/QuickGen.pm
@@ -1,6 +1,8 @@
 package Test::QuickGen;
 
 use v5.40;
+use strict;
+use warnings;
 use Exporter 'import';
 
 our $VERSION = '0.1.0';
@@ -24,6 +26,10 @@ Utilities for generating random data.
 
 =head2 id
 
+  my $new_id = id();
+  my $newer_id = id();
+  print $new_id == $newer_id; # 0
+
 Generate a program unique ID. Will reset on each program run.
 
 =cut
@@ -35,11 +41,14 @@ sub id {
 
 =head2 string_of
 
+  print string_of(10, qw(a b c d)); # aaabcbdabd
+
 Generate a random string of N length with the given characters.
 
 =cut
 
-sub string_of ($n, @chars) {
+sub string_of {
+  my ($n, @chars) = @_;
   my $str;
   for (1..$n) {
     $str .= $chars[rand @chars];
@@ -49,11 +58,14 @@ sub string_of ($n, @chars) {
 
 =head2 ascii_string
 
+  print ascii_string(10); # aZfjar190a
+
 Generate a random ASCII string of N length.
 
 =cut
 
-sub ascii_string ($n) {
+sub ascii_string {
+  my ($n) = @_;
   string_of($n, 'a'..'z', 'A'..'Z', '0'..'9', '_');
 }
 
@@ -63,7 +75,8 @@ Generate a random UTF-8 string of N length.
 
 =cut
 
-sub utf8_string ($n) {
+sub utf8_string {
+  my ($n) = @_;
   my $str = '';
   while (length($str) < $n) {
     # skip non-visible ASCII characters (0x00-0x19)
@@ -88,7 +101,8 @@ A clean UTF-8 string that is absent of special characters.
 
 =cut
 
-sub utf8_sanitized ($n) {
+sub utf8_sanitized {
+  my ($n) = @_;
   my $s = utf8_string($n);
   $s =~ s/[^\p{L}\p{N}\s]//gu;
 
@@ -106,28 +120,38 @@ Generate a string of N words, using the string generator C<$gen>.
 
 =cut
 
-sub words ($gen, $n) {
+sub words {
+  my ($gen, $n) = @_;
   my @words = map { $gen->(between(1, 70)) } (1..$n);
   join ' ', @words;
 }
 
 =head2 between
 
+  print between(1, 10); # 1
+  print between(1, 10); # 10
+  print between(1, 10); # 8
+
 Return a integer between min and max (inclusive).
 
 =cut
 
-sub between ($min, $max) {
+sub between {
+  my ($min, $max) = @_;
   $min + int(rand($max - $min + 1));
 }
 
 =head2 nullable
 
+  print nullable(2); # 2
+  print nullable(2); # undef
+
 Has a chance of nulling the given value.
 
 =cut
 
-sub nullable ($val) {
+sub nullable {
+  my ($val) = @_;
   if (rand() < 0.25) {
     undef;
   } else {
@@ -136,6 +160,9 @@ sub nullable ($val) {
 }
 
 =head2 pick
+
+  print pick(1, 2, 3); # 3
+  print pick(1, 2, 3); # 1
 
 Pick a random element from a list.
 
